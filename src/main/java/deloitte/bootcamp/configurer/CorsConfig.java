47790b450d18acb4b8dc74b4118761742b7f01bb
Arthur Silveira <arthurmarques2148@gmail.com>
2026-02-06 17:10:01 -0300

    conf: Ajusta o cors para o swagger rodar no azure

---
+++ src/main/java/deloitte/bootcamp/configurer/CorsConfig.java
@@ -0,0 +1,26 @@
+package deloitte.bootcamp.configurer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://bootcamp-produtos-arthurmarques-ethrh3hgakawajb7.brazilsouth-01.azurewebsites.net"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
